e41e85a6f827914b2d0d04e942830b04c0ded743
George Lamb <georgeashleylamb@gmail.com>
2021-03-01 18:26:13 +1100

    Add basic quad rendering

--- quo.h
+++ quo.h
@@ -60,6 +60,7 @@ typedef HGLRC quo_GLRenderContext;
 #endif
 
 /* GL load types */
+typedef int CALLSTYLE quo_gl_get_uniform_location(unsigned int, const char*);
 typedef unsigned int CALLSTYLE quo_gl_create_program();
 typedef unsigned int CALLSTYLE quo_gl_create_shader(unsigned int);
 typedef void CALLSTYLE quo_gl_attach_shader(unsigned int, unsigned int);
@@ -67,6 +68,7 @@ typedef void CALLSTYLE quo_gl_bind_buffer(unsigned int, int);
 typedef void CALLSTYLE quo_gl_bind_vertex_array(unsigned int);
 typedef void CALLSTYLE quo_gl_buffer_data(unsigned int, int, const void*, unsigned int);
 typedef void CALLSTYLE quo_gl_compile_shader(unsigned int);
+typedef void CALLSTYLE quo_gl_delete_program(unsigned int*);
 typedef void CALLSTYLE quo_gl_delete_shader(unsigned int);
 typedef void CALLSTYLE quo_gl_enable_vertex_attrib_array(unsigned int);
 typedef void CALLSTYLE quo_gl_gen_buffers(unsigned int, unsigned int*);
@@ -77,8 +79,14 @@ typedef void CALLSTYLE quo_gl_get_shader_info_log(unsigned int, int, int, char*)
 typedef void CALLSTYLE quo_gl_get_shader_iv(unsigned int, unsigned int, int*);
 typedef void CALLSTYLE quo_gl_link_program(unsigned int);
 typedef void CALLSTYLE quo_gl_shader_source(unsigned int, int, const char**, const int*);
+typedef void CALLSTYLE quo_gl_uniform_1_f(int, float);
+typedef void CALLSTYLE quo_gl_uniform_1_i(int, int);
+typedef void CALLSTYLE quo_gl_uniform_2_f(int, float, float);
+typedef void CALLSTYLE quo_gl_uniform_3_f(int, float, float, float);
+typedef void CALLSTYLE quo_gl_uniform_4_f(int, float, float, float, float);
+typedef void CALLSTYLE quo_gl_uniform_matrix_4_f_v(int, int, bool, float*);
 typedef void CALLSTYLE quo_gl_use_program(unsigned int);
-typedef void CALLSTYLE quo_gl_vertex_attrib_pointer(unsigned int, int, unsigned int, GLboolean, int, const void*);
+typedef void CALLSTYLE quo_gl_vertex_attrib_pointer(unsigned int, int, unsigned int, bool, int, const void*);
 
 /* OpenGL functions */
 quo_gl_attach_shader* glAttachShader = NULL;
@@ -88,6 +96,7 @@ quo_gl_buffer_data* glBufferData = NULL;
 quo_gl_compile_shader* glCompileShader = NULL;
 quo_gl_create_program* glCreateProgram = NULL;
 quo_gl_create_shader* glCreateShader = NULL;
+quo_gl_delete_program* glDeleteProgram = NULL;
 quo_gl_delete_shader* glDeleteShader = NULL;
 quo_gl_enable_vertex_attrib_array* glEnableVertexAttribArray = NULL;
 quo_gl_gen_buffers* glGenBuffers = NULL;
@@ -96,8 +105,15 @@ quo_gl_get_program_info_log* glGetProgramInfoLog = NULL;
 quo_gl_get_program_iv* glGetProgramiv = NULL;
 quo_gl_get_shader_info_log* glGetShaderInfoLog = NULL;
 quo_gl_get_shader_iv* glGetShaderiv = NULL;
+quo_gl_get_uniform_location* glGetUniformLocation = NULL;
 quo_gl_link_program* glLinkProgram = NULL;
 quo_gl_shader_source* glShaderSource = NULL;
+quo_gl_uniform_1_f* glUniform1f = NULL;
+quo_gl_uniform_1_i* glUniform1i = NULL;
+quo_gl_uniform_2_f* glUniform2f = NULL;
+quo_gl_uniform_3_f* glUniform3f = NULL;
+quo_gl_uniform_4_f* glUniform4f = NULL;
+quo_gl_uniform_matrix_4_f_v* glUniformMatrix4fv = NULL;
 quo_gl_use_program* glUseProgram = NULL;
 quo_gl_vertex_attrib_pointer* glVertexAttribPointer = NULL;
 
@@ -130,9 +146,9 @@ typedef struct quo_Window {
 #endif
 } quo_Window;
 
+/* Window management */
 void quo_init_window(quo_Window* window, int w, int h);
 void quo_set_window_title(quo_Window* window, const char* title);
-void quo_clear_window(quo_Window* window, int r, int g, int b);
 void quo_update_window(quo_Window* window);
 void quo_free_window(quo_Window* window);
 
@@ -141,17 +157,87 @@ void quo_free_window(quo_Window* window);
  * -----------------------*/
 
  /* -----------------------
+ * START MATHS
+ * -----------------------*/
+/* A 4x4 matrix structure, and it's operational functions */
+typedef struct quo_Matrix {
+	float elements[16];
+} quo_Matrix;
+
+quo_Matrix quo_identity();
+quo_Matrix quo_translate(quo_Matrix m, float x, float y, float z);
+quo_Matrix quo_scale(quo_Matrix m, float x, float y, float z);
+quo_Matrix quo_orthographic(float left, float right, float bottom, float top, float near, float far);
+/* -----------------------
+* END MATHS
+* -----------------------*/
+
+/* Generic rectangle struct */
+typedef struct quo_Rect {
+	int x, y, w, h;
+} quo_Rect;
+
+/* -----------------------
  * START RENDERER
  * -----------------------*/
 
-// typedef struct quo_Renderer {
-// } quo_Renderer;
+#define QUO_MAX_SHADERS 10
 
- /* -----------------------
+/* To be used as an index into the renderer's shader array */
+typedef unsigned int quo_ShaderHandle;
+
+/* Responsible for drawing things to the screen */
+typedef struct quo_Renderer {
+	quo_Window* window;
+	quo_Matrix projection;
+
+	quo_ShaderHandle sprite_shader;
+
+	unsigned int quad_va;
+
+	unsigned int shaders[QUO_MAX_SHADERS];
+	unsigned int shader_count;
+} quo_Renderer;
+
+/* Rendering utility */
+/* Create a hex colour from red, green, and blue values*/
+unsigned long quo_color_from_rgb(int r, int g, int b);
+
+/* Renderer management */
+void quo_init_renderer(quo_Renderer* renderer, quo_Window* window);
+void quo_free_renderer(quo_Renderer* renderer);
+void quo_update_renderer(quo_Renderer* renderer);
+
+/* Draw functions */
+void quo_clear_renderer(unsigned long color);
+void quo_draw_quad(quo_Renderer* renderer, quo_Rect src, quo_Rect dest, unsigned long color);
+
+/* Shader management */
+quo_ShaderHandle quo_create_shader(quo_Renderer* renderer, const char* vertex_source, const char* fragment_source);
+void quo_bind_shader(quo_Renderer* renderer, quo_ShaderHandle shader);
+void quo_shader_set_color(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, unsigned long color);
+void quo_shader_set_float(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float val);
+void quo_shader_set_int(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, int val);
+void quo_shader_set_matrix(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, quo_Matrix m);
+void quo_shader_set_vec2(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float x, float y);
+void quo_shader_set_vec3(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float x, float y, float z);
+void quo_shader_set_vec4(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float x, float y, float z, float w);
+
+/* -----------------------
  * END RENDERER
  * -----------------------*/
 
-/* Implementation */
+
+/*  _____                 _                           _        _   _
+ * |_   _|               | |                         | |      | | (_)
+ *   | |  _ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _| |_ _  ___  _ __
+ *   | | | '_ ` _ \| '_ \| |/ _ \ '_ ` _ \ / _ \ '_ \| __/ _` | __| |/ _ \| '_ \
+ *  _| |_| | | | | | |_) | |  __/ | | | | |  __/ | | | || (_| | |_| | (_) | | | |
+ * |_____|_| |_| |_| .__/|_|\___|_| |_| |_|\___|_| |_|\__\__,_|\__|_|\___/|_| |_|
+ *                 | |
+ *                 |_|
+ */
+
 #ifdef QUO_IMPL
 /* C standard includes */
 #include <assert.h>
@@ -166,6 +252,7 @@ void quo_load_gl() {
 	glCompileShader = QUO_LOAD_GL_FUNC(quo_gl_compile_shader, "glCompileShader");
 	glCreateProgram = QUO_LOAD_GL_FUNC(quo_gl_create_program, "glCreateProgram");
 	glCreateShader = QUO_LOAD_GL_FUNC(quo_gl_create_shader, "glCreateShader");
+	glDeleteProgram = QUO_LOAD_GL_FUNC(quo_gl_delete_program, "glDeleteProgram");
 	glDeleteShader = QUO_LOAD_GL_FUNC(quo_gl_delete_shader, "glDeleteShader");
 	glEnableVertexAttribArray = QUO_LOAD_GL_FUNC(quo_gl_enable_vertex_attrib_array, "glEnableVertexAttribArray");
 	glGenBuffers = QUO_LOAD_GL_FUNC(quo_gl_gen_buffers, "glGenBuffers");
@@ -174,8 +261,15 @@ void quo_load_gl() {
 	glGetProgramiv = QUO_LOAD_GL_FUNC(quo_gl_get_program_iv, "glGetProgramiv");
 	glGetShaderInfoLog = QUO_LOAD_GL_FUNC(quo_gl_get_shader_info_log, "glGetShaderInfoLog");
 	glGetShaderiv = QUO_LOAD_GL_FUNC(quo_gl_get_shader_iv, "glGetShaderiv");
+	glGetUniformLocation = QUO_LOAD_GL_FUNC(quo_gl_get_uniform_location, "glGetUniformLocation");
 	glLinkProgram = QUO_LOAD_GL_FUNC(quo_gl_link_program, "glLinkProgram");
 	glShaderSource = QUO_LOAD_GL_FUNC(quo_gl_shader_source, "glShaderSource");
+	glUniform1f = QUO_LOAD_GL_FUNC(quo_gl_uniform_1_f, "glUniform1f");
+	glUniform1i = QUO_LOAD_GL_FUNC(quo_gl_uniform_1_i, "glUniform1i");
+	glUniform2f = QUO_LOAD_GL_FUNC(quo_gl_uniform_2_f, "glUniform2f");
+	glUniform3f = QUO_LOAD_GL_FUNC(quo_gl_uniform_3_f, "glUniform3f");
+	glUniform4f = QUO_LOAD_GL_FUNC(quo_gl_uniform_4_f, "glUniform4f");
+	glUniformMatrix4fv = QUO_LOAD_GL_FUNC(quo_gl_uniform_matrix_4_f_v, "glUniformMatrix4fv");
 	glUseProgram = QUO_LOAD_GL_FUNC(quo_gl_use_program, "glUseProgram");
 	glVertexAttribPointer = QUO_LOAD_GL_FUNC(quo_gl_vertex_attrib_pointer, "glVertexAttribPointer");
 }
@@ -295,17 +389,10 @@ void quo_set_window_title(quo_Window* window, const char* title) {
 #if defined(QUO_PLATFORM_WINDOWS)
 	SetWindowText(window->hwnd, title);
 #endif
-}
 
-void quo_clear_window(quo_Window* window, int r, int g, int b) {
-	assert(window != NULL);
-
-	float rf = (float)r / 255.0f;
-	float gf = (float)g / 255.0f;
-	float bf = (float)b / 255.0f;
-
-	glClearColor(rf, gf, bf, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+#if defined(QUO_PLATFORM_X11)
+	XStoreName(window->display, window->window, title);
+#endif
 }
 
 void quo_update_window(quo_Window* window) {
@@ -365,10 +452,286 @@ void quo_free_window(quo_Window* window) {
  * -----------------------*/
 
  /* -----------------------
- * START RENDERER
+ * START MATHS
+ * -----------------------*/
+quo_Matrix quo_identity() {
+	quo_Matrix result;
+
+	memset(result.elements, 0, 16 * sizeof(float));
+
+	result.elements[0 + 0 * 4] = 1.0f;
+	result.elements[1 + 1 * 4] = 1.0f;
+	result.elements[2 + 2 * 4] = 1.0f;
+	result.elements[3 + 3 * 4] = 1.0f;
+
+	return result;
+}
+
+quo_Matrix quo_translate(quo_Matrix m, float x, float y, float z) {
+	m.elements[3 + 0 * 4] = x;
+	m.elements[3 + 1 * 4] = y;
+	m.elements[3 + 2 * 4] = z;
+
+	return m;
+}
+
+quo_Matrix quo_scale(quo_Matrix m, float x, float y, float z) {
+	m.elements[0 + 0 * 4] = x;
+	m.elements[1 + 1 * 4] = y;
+	m.elements[2 + 2 * 4] = z;
+
+	return m;
+}
+
+quo_Matrix quo_orthographic(float left, float right, float bottom, float top, float near, float far) {
+	quo_Matrix result = quo_identity();
+
+	result.elements[0 + 0 * 4] = 2.0f / (right - left);
+	result.elements[1 + 1 * 4] = 2.0f / (top - bottom);
+	result.elements[2 + 2 * 4] = 2.0f / (near - far);
+
+	result.elements[3 + 0 * 4] = (left + right) / (left - right);
+	result.elements[3 + 1 * 4] = (bottom + top) / (bottom - top);
+	result.elements[3 + 2 * 4] = (far + near) / (far - near);
+
+	return result;
+}
+ /* -----------------------
+ * END MATHS
  * -----------------------*/
 
  /* -----------------------
+ * START RENDERER
+ * -----------------------*/
+
+static const char* g_quad_shader_vertex = "#version 330 core\n"
+"layout (location = 0) in vec4 vertex;\n"
+"uniform mat4 projection = mat4(1.0);\n"
+"uniform mat4 model = mat4(1.0);\n"
+"uniform mat4 view = mat4(1.0);\n"
+"void main() {\n"
+"gl_Position = projection * view * model * vec4(vertex.xy, 0.0, 1.0);\n"
+"}\n";
+
+static const char* g_quad_shader_fragment = "#version 330 core\n"
+"out vec4 out_color;\n"
+"uniform vec3 color = vec3(1.0);\n"
+"void main() {\n"
+"out_color = vec4(color, 1.0);\n"
+"}\n";
+
+static void check_shader_errors(unsigned int shader) {
+	int r;
+	char info_log[1024];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &r);
+	if (!r) {
+		glGetShaderInfoLog(shader, 1024, 0, info_log);
+		fprintf(stderr, "Shader error %s\n", info_log);
+	}
+}
+
+unsigned long quo_color_from_rgb(int r, int g, int b) {
+	return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
+
+void quo_init_renderer(quo_Renderer* renderer, quo_Window* window) {
+	assert(renderer != NULL);
+	assert(window != NULL);
+
+	renderer->shader_count = 0;
+	renderer->window = window;
+
+	for (unsigned int i = 0; i < QUO_MAX_SHADERS; i++) {
+		renderer->shaders[i] = 0;
+	}
+
+	renderer->projection = quo_orthographic(0.0f, window->width, window->height, 0.0f, -1.0f, 1.0f);
+
+	renderer->sprite_shader = quo_create_shader(renderer, g_quad_shader_vertex, g_quad_shader_fragment);
+
+	unsigned int quad_vb;
+	float vertices[] = {
+		0.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+
+		0.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1, &renderer->quad_va);
+	glGenBuffers(1, &quad_vb);
+
+	glBindBuffer(GL_ARRAY_BUFFER, quad_vb);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(renderer->quad_va);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), NULL);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void quo_update_renderer(quo_Renderer* renderer) {
+	assert(renderer != NULL);
+	assert(renderer->window != NULL);
+
+	renderer->projection = quo_orthographic(0.0f, renderer->window->width, renderer->window->height, 0.0f, -1.0f, 1.0f);
+}
+
+void quo_draw_quad(quo_Renderer* renderer, quo_Rect src, quo_Rect dest, unsigned long color) {
+	assert(renderer != NULL);
+
+	quo_Matrix model = quo_identity();
+	model = quo_translate(model, dest.x, dest.y, 0);
+	model = quo_scale(model, dest.w, dest.h, 1);
+
+	quo_bind_shader(renderer, renderer->sprite_shader);
+
+	quo_shader_set_matrix(renderer, renderer->sprite_shader, "model", model);
+	quo_shader_set_matrix(renderer, renderer->sprite_shader, "projection", renderer->projection);
+	quo_shader_set_color(renderer, renderer->sprite_shader, "color", color);
+
+	glBindVertexArray(renderer->quad_va);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+
+void quo_free_renderer(quo_Renderer* renderer) {
+	assert(renderer != NULL);
+
+	for (unsigned int i = 0; i < renderer->shader_count; i++) {
+		glDeleteProgram(&renderer->shaders[i]);
+	}
+}
+
+void quo_clear_renderer(unsigned long color) {
+	int r = (color >> 16) & 0xFF;
+	int g = (color >> 8) & 0xFF;
+	int b = color & 0xFF;
+
+	float rf = (float)r / 255.0f;
+	float gf = (float)g / 255.0f;
+	float bf = (float)b / 255.0f;
+
+	glClearColor(rf, gf, bf, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+quo_ShaderHandle quo_create_shader(quo_Renderer* renderer, const char* vertex_source, const char* fragment_source) {
+	assert(renderer != NULL);
+
+	if (renderer->shader_count > QUO_MAX_SHADERS) {
+		fprintf(stderr, "Failed to create shader: Shader count too high (max %d)\n", QUO_MAX_SHADERS);
+		return 0;
+	}
+
+	unsigned int program = glCreateProgram();
+
+	unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_source, NULL);
+	glCompileShader(vertex_shader);
+	check_shader_errors(vertex_shader);
+
+	unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_source, NULL);
+	glCompileShader(fragment_shader);
+	check_shader_errors(fragment_shader);
+
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	unsigned int index = renderer->shader_count++;
+
+	renderer->shaders[index] = program;
+
+	return index;
+}
+
+void quo_bind_shader(quo_Renderer* renderer, quo_ShaderHandle shader) {
+	assert(renderer != NULL);
+
+	glUseProgram(renderer->shaders[shader]);
+}
+
+void quo_shader_set_color(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, unsigned long color) {
+	int r = (color >> 16) & 0xFF;
+	int g = (color >> 8) & 0xFF;
+	int b = color & 0xFF;
+
+	float rf = (float)r / 255.0f;
+	float gf = (float)g / 255.0f;
+	float bf = (float)b / 255.0f;
+
+	quo_shader_set_vec3(renderer, shader, uniform_name, rf, gf, bf);
+}
+
+void quo_shader_set_float(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float val) {
+	assert(renderer != NULL);
+	assert(shader < renderer->shader_count);
+
+	unsigned int shader_id = renderer->shaders[shader];
+	unsigned int location = glGetUniformLocation(shader_id, uniform_name);
+
+	glUniform1f(location, val);
+}
+
+void quo_shader_set_int(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, int val) {
+	assert(renderer != NULL);
+	assert(shader < renderer->shader_count);
+
+	unsigned int shader_id = renderer->shaders[shader];
+	unsigned int location = glGetUniformLocation(shader_id, uniform_name);
+
+	glUniform1i(location, val);
+}
+
+void quo_shader_set_matrix(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, quo_Matrix m) {
+	assert(renderer != NULL);
+	assert(shader < renderer->shader_count);
+
+	unsigned int shader_id = renderer->shaders[shader];
+	unsigned int location = glGetUniformLocation(shader_id, uniform_name);
+
+	glUniformMatrix4fv(location, 1, true, m.elements);
+}
+
+void quo_shader_set_vec2(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float x, float y) {
+	assert(renderer != NULL);
+	assert(shader < renderer->shader_count);
+
+	unsigned int shader_id = renderer->shaders[shader];
+	unsigned int location = glGetUniformLocation(shader_id, uniform_name);
+
+	glUniform2f(location, x, y);
+}
+
+void quo_shader_set_vec3(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float x, float y, float z) {
+	assert(renderer != NULL);
+	assert(shader < renderer->shader_count);
+
+	unsigned int shader_id = renderer->shaders[shader];
+	unsigned int location = glGetUniformLocation(shader_id, uniform_name);
+
+	glUniform3f(location, x, y, z);
+}
+
+void quo_shader_set_vec4(quo_Renderer* renderer, quo_ShaderHandle shader, const char* uniform_name, float x, float y, float z, float w) {
+	assert(renderer != NULL);
+	assert(shader < renderer->shader_count);
+
+	unsigned int shader_id = renderer->shaders[shader];
+	unsigned int location = glGetUniformLocation(shader_id, uniform_name);
+
+	glUniform4f(location, x, y, z, w);
+}
+
+/* -----------------------
  * END RENDERER
  * -----------------------*/
 
--- test.c
+++ test.c
@@ -1,99 +1,25 @@
 #define QUO_IMPL
 #include "quo.h"
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
-
 int main() {
 	quo_Window window;
 
 	quo_init_window(&window, 640, 480);
 	quo_set_window_title(&window, "quo example program");
 
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, 0, infoLog);
-        printf("%s\n", infoLog);
-    }
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, 0, infoLog);
-        printf("%s\n", infoLog);
-    }
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, 0, infoLog);
-        printf("%s\n", infoLog);
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-	float vertices[] = {
-    		-0.5f, -0.5f, 0.0f,
-     		0.5f, -0.5f, 0.0f,
-     		0.0f,  0.5f, 0.0f
-	};
-
-	    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-
+	quo_Renderer renderer;
+	quo_init_renderer(&renderer, &window);
 
 	while (window.is_open) {
-		quo_clear_window(&window, 100, 100, 100);
+		quo_clear_renderer(0x000000);
 
-		        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+		quo_draw_quad(&renderer, (quo_Rect){}, (quo_Rect){100, 100, 50, 50}, 0xbd4d4d);
 
-
+		quo_update_renderer(&renderer);
 		quo_update_window(&window);
 	}
+
+	quo_free_renderer(&renderer);
 
 	quo_free_window(&window);
 }
