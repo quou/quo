c870b104d01eba7b3e5d994a0e18d9c707d7b075
George Lamb <georgeashleylamb@gmail.com>
2021-03-10 19:45:31 +1100

    Add example to top of header

--- quo/quo.h
+++ quo/quo.h
@@ -21,6 +21,52 @@
  * IN THE SOFTWARE.
  */
 
+/* Before use, you must #define
+ * 	QUO_IMPL
+ * in *one* c/cpp file, to provide an implementation.*/
+
+/* COMPLETE EXAMPLE to render a red rectangle on the screen */
+#if 0
+#define QUO_IMPL
+#include <quo.h>
+
+int main() {
+	/* Create window */
+	quo_Window window;
+	quo_init_window(&window, 640, 480, false);
+	quo_set_window_title(&window, "quo example program");
+
+	/* Create renderer */
+	quo_Renderer renderer;
+	quo_init_renderer(&renderer, &window);
+
+	/* Game loop */
+	while (window.is_open) {
+		/* Update window event */
+		quo_update_window_events(&window);
+
+		/* Clear to a solid black colour */
+		quo_clear_renderer(0x000000);
+
+		/* Draw a red rectangle */
+		quo_draw_rect(&renderer, (quo_Rect){100, 100, 50, 50}, 0xff1100);
+
+		/* Print the FPS when the space key is pressed */
+		if (quo_key_pressed(QUO_KEY_SPACE)) {
+			printf("FPS: %g\n", window.fps);
+		}
+
+		/* Update window and renderer */
+		quo_update_renderer(&renderer);
+		quo_update_window(&window);
+	}
+
+	/* Clean up */
+	quo_free_renderer(&renderer);
+	quo_free_window(&window);
+}
+#endif
+
 /* Make sure it works with C++, too */
 #ifdef __cplusplus
 extern "C" {
@@ -713,7 +759,7 @@ static LRESULT CALLBACK quo_win32_event_callback(HWND hwnd, UINT msg, WPARAM wpa
 		case WM_KILLFOCUS:
 			q_window->is_focused = false;
 			return 0;
-		case WM_LBUTTONDOWN: 
+		case WM_LBUTTONDOWN:
 			if (q_window->is_focused) {
 				i_quo_set_mouse_button_held_state(QUO_MOUSE_BUTTON_1, true);
 				i_quo_set_mouse_button_down_state(QUO_MOUSE_BUTTON_1, true);
