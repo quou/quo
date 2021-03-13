5fa20eb554c85f83c0003a2ebc1d7aa7d360b848
George Lamb <georgeashleylamb@gmail.com>
2021-03-13 20:34:22 +1100

    Add simple IMGUI

--- sbox/src/sbox.c
+++ sbox/src/sbox.c
@@ -18,20 +18,36 @@ int main() {
 	quo_Renderer renderer;
 	quo_init_renderer(&renderer, &window);
 
+	quo_init_imgui(&renderer);
+	quo_imgui_use_default_settings();
+
+	char fps_string[128];
 	while (window.is_open) {
 		quo_update_window_events(&window);
 
 		quo_clear_renderer(0x000000);
 
-		quo_draw_texture(&renderer, &texture, (quo_Rect){0, 0, 13, 13}, (quo_Rect){100, 100, 50, 50}, 0xffffff);
+		quo_draw_texture(&renderer, &texture, (quo_Rect){0, 0, 13, 13}, (quo_Rect){400, 250, 50, 50}, 0xffffff);
 
-		if (quo_key_just_pressed(QUO_KEY_SPACE)) {
-			printf("%g\n", window.fps);
+		if (quo_imgui_button("Print FPS")) {
+			sprintf(fps_string, "%g\n", window.fps);
 		}
+
+		quo_imgui_text(fps_string);
+		quo_imgui_text("Hey there. Welcome to Quo, a single-header,\nzero dependency game library written in C.\nThis library is standalone, meaning it doesn't\ndepend on anything that doesn't come with\nthe operating system.");
+
+		if (quo_imgui_button("I'm a button!")) {
+			sprintf(fps_string, "%g\n", window.fps);
+		}
+
+
+		quo_imgui_end_frame();
 
 		quo_update_renderer(&renderer);
 		quo_update_window(&window);
 	}
+
+	quo_free_imgui();
 
 	quo_free_texture(&texture);
 	quo_free_renderer(&renderer);
